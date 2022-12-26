411a3322b3f8ff2160f2af80ad7658fd479764f3
Lilian Crivelli <liliandevsistem@gmail.com>
2022-12-26 12:55:09 -0400

    Udemy - curse de JS

---
+++ Seção 2 _ JS basico para iniciantes/Aula1.js
@@ -0,0 +1,10 @@
+console.log("Prazer, Lilian Crivelli!");
+console.log('Prazer, Lilian Crivelli!');
+console.log(`'Luiz' "otavio"`);
+console.log(35, 45, 36, 15.45655, "LUX");
+
+console.log('João nasceu em 1984.');
+console.log('Em 2000 João conheceu Maria.');
+console.log('João casou-se com Maria em 2012.');
+console.log('Maria teve um filho com João em 2015.');
+console.log('O filho de João chama-se Eduardo.');
---
+++ Seção 2 _ JS basico para iniciantes/Ex_aula1.js
@@ -0,0 +1,4 @@
+console.log('Meu nome é "LILIAN", estou aprendendo js à', 01, 'da madrugada.');
+//console.log('Meu nome é "LILIAN", estou aprendendo js à 01 da madrugada.')
+//O JS não lê o número dentro do parenteses como número e  sim como texto.
+// Para o JS ler o número como número a forma correta está descrita na linha 01.
