90af64675e26a3193380f1bfadca044f7e87cf53
Lilian Crivelli <liliandevsistem@gmail.com>
2023-01-04 11:50:28 -0400

    Algumas regras de JS

--- Seção 2 _ JS basico para iniciantes/Aula2.js
+++ Seção 2 _ JS basico para iniciantes/Aula2.js
@@ -1,7 +1,37 @@
-let nome = 'João';
+let nome; //Declarou a variável
+nome = 'João'; //Inicializando a variável
+console.log(nome);
+nome = 'Otávio';
+console.log(nome);
 
-console.log(nome, 'nasceu em 1984.');
-console.log('Em 2000', nome, 'conheceu Maria.');
-console.log(nome, 'casou-se com Maria em 2012.');
-console.log('Maria teve um filho com', nome, 'em 2015.');
-console.log('O filho de', nome, 'chama-se Eduardo.');
+// Se eu estiver utilizando 'let' não posso re-declarar a variável, isso gera erro;
+
+//Não podemos criar uma variável com palavras reservadas da linguagem JS;
+
+//Precisam ter nomes significativos;
+//Nãp se começa nome de variável com números;
+
+//Não podemos usar essas palavras reservadas como variáveis, labels ou nomes de função:
+//abstract 	arguments 	await 	boolean
+//break 	byte 	case 	catch
+//char 	class 	const 	continue
+//debugger 	default 	delete 	do
+//double 	else 	enum 	eval
+//export 	extends 	false 	final
+//finally 	float 	for 	function
+//goto 	if 	implements 	import
+//in 	instanceof 	int 	interface
+//let 	 long 	native 	new
+//null 	package 	private 	protected
+//public 	return 	short 	static
+//super 	 switch 	synchronized 	this
+//throw 	throws 	transient 	true
+//try 	typeof 	var 	void
+//volatile 	while 	with 	yield
+
+//Devemos evitar também usar o nome de objetos, propriedades e métodos integrados do JavaScript
+//Array 	Date 	eval 	function
+//hasOwnProperty 	Infinity 	isFinite 	isNaN
+//isPrototypeOf 	 length 	Math 	NaN
+//name 	Number 	Object 	prototype
+//String 	toString 	Undefined 	valueOf
